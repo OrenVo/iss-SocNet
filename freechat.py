9d8d3f081786471d6646c965b81c879c3b2a7e9d
xfulla00 <xfulla00@stud.fit.vutbr.cz>
2020-11-25 03:55:42 +0100

    Added few more routes

--- freechat.py
+++ freechat.py
@@ -16,15 +16,19 @@ from src.db import DB, init_db, User
 from src.error import eprint
 from collections import namedtuple
 from datetime import timedelta
-from flask import Flask, redirect, render_template, request, url_for, session
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_login import current_user, LoginManager, login_required, login_user, logout_user, UserMixin
 
 '''
 TODO List
+Login potencialna nekonzistencia - vymaze ucet pocas prihlasovania
 Profile picture
-Dano's file
-https://stackoverflow.com/questions/50143672/passing-a-variable-from-jinja2-template-to-route-in-flask
 Escape links & other input to prevent crashes and hijacking
+
+/settings iba
+Dano ja ti mozem posielat cele objekty
+https://stackoverflow.com/questions/50143672/passing-a-variable-from-jinja2-template-to-route-in-flask
+Dano's file
 '''
 
 # App initialization #
@@ -41,7 +45,7 @@ login_manager.login_view = "welcome"
 # Pages
 ################################################################################
 
-# Visitor pages #
+# Visitors #
 
 @app.route("/")
 @app.route('/index/')
@@ -60,20 +64,36 @@ def welcome():
 def register():
     if current_user.is_authenticated:
         return redirect(url_for("lost"))
-    if request.method == 'POST':
-        login = request.form['login']
-        password = request.form['psw']
-        repeat = request.form['psw-repeat']
-        if not db.check_username(login):
-            # TODO add message what was wrong
-            return render_template("registration_page.html")
-        if password != repeat:
-            # TODO add message what was wrong & keep the username
-            return render_template("registration_page.html")
-        db.insert_new_user(login, password)
-        # TODO add message that it was succesful
+    if request.method == 'GET':
+        return render_template("registration_page.html")
+    login = request.form['login']
+    password = request.form['psw']
+    repeat = request.form['psw-repeat']
+    if not db.check_username(login):
+        # TODO add message what was wrong
+        return render_template("registration_page.html")
+    if password != repeat:
+        # TODO add message what was wrong & keep the username
+        return render_template("registration_page.html")
+    db.insert_new_user(login, password)
+    # TODO add message that it was succesful
+    return redirect(url_for("welcome"))
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        if current_user.is_authenticated:
+            return redirect(url_for("lost"))
         return redirect(url_for("welcome"))
-    return render_template("registration_page.html")
+    login = request.form["uname"]
+    password = request.form["psw"]
+    if not db.check_password(password, login):
+        # TODO add message that login was unsuccesful & keep username
+        return redirect(url_for("welcome"))
+    user = User.query.filter_by(Login=login).first()
+    login_user(user)
+    return redirect(url_for("home"))
 
 
 @app.route("/browse/")
@@ -84,7 +104,7 @@ def guest():
         return render_template("guest_page.html")
 
 
-# User pages #
+# Users #
 
 @app.route("/home/")
 @login_required
@@ -98,39 +118,158 @@ def home():
     return render_template("home_page.html", username=username, rights="user", img_src=picture)
 
 
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("welcome"))
+
+
 @app.route("/profile/<name>/")
 @app.route("/user/<name>/")
 @app.route("/users/<name>/")
 @app.route("/profiles/<name>/")
 def profile(name):
-    user = name
+    user = User.query.filter_by(Login=name).first()
     private = user.Mode & 1
     if private and current_user.is_anonymous:
         return render_template("tresspassing_page.html")
     return render_template("profile_page.html", username=name)
 
 
+@app.route("/settings")
+@login_required
+def profile_settings():
+    # TODO return settings
+    return current_user.username + " settings page."
+
+
+@app.route("/profile/<name>/settings")
+@app.route("/user/<name>/settings")
+@app.route("/users/<name>/settings")
+@app.route("/profiles/<name>/settings")
+@login_required
+def profile_settings_two(name):
+    if current_user.username != name:
+        return render_template("tresspassing_page.html")
+    return redirect(url_for("profile_settings"))
+
+
+# Groups #
+
+@app.route("/create/group/")
+@login_required
+def create_group():
+    # TODO
+    pass
+
+
+@app.route("/create/thread/<name>/")
+@login_required
+def create_thread():
+    # TODO
+    pass
+
+
+@app.route("/group/<group>/<thread>/new")
+@app.route("/groups/<group>/<thread>/new")
+@login_required
+def send_message(group, thread):
+    # TODO
+    pass
+
+
+@login_required
+def increment(name):
+    pass
+
+
+@login_required
+def decrement(name):
+    pass
+
+
 @app.route("/group/<name>/")
 @app.route("/groups/<name>/")
 def group(name):
-    group = name
+    group = Group.query.filter_by(Name=name).first()
     private = group.Mode & 1
     if private and current_user.is_anonymous:
         return render_template("tresspassing_page.html")
     return render_template("group_page.html", groupname=name)
 
 
-@app.route("/group/<name>/<thread>")
-@app.route("/groups/<name>/<thread>")
-def thread(name, thread):
-    group = name
+@app.route("/group/<group>/<thread>")
+@app.route("/groups/<group>/<thread>")
+def see_posts(group, thread):
+    group = Group.query.filter_by(Name=name).first()
     private = group.Mode & 1
     if private and current_user.is_anonymous:
         return render_template("tresspassing_page.html")
     return render_template("thread_page.html", groupname=name, threadname=thread)
 
 
-# Other pages #
+@app.route("/group/<name>/settings")
+@app.route("/groups/<name>/settings")
+@login_required
+def group_settings(name):
+    group = Group.query.filter_by(Name=name).first()
+    if current_user.ID != group.User_ID:
+        return render_template("tresspassing_page.html")
+    # TODO return settings
+    return name + " settings page."
+
+
+@app.route("/group/<name>/notifications")
+@app.route("/groups/<name>/notifications")
+@login_required
+def group_notifications(name):
+    pass
+
+
+@login_required
+def ask_mem(name):
+    pass
+
+
+@login_required
+def ask_mod(name):
+    pass
+
+
+# Moderation #
+
+@login_required
+def delete(name):
+    pass
+
+
+@login_required
+def kick(name):
+    pass
+
+
+@login_required
+def ban(name):
+    pass
+
+# Other #
+
+# TODO WE don't know if it works
+@app.route('/search', methods=['GET'])
+def search():
+    return flask.render_template('search.html')
+
+
+# TODO WE don't know if it works
+@app.route('/search_result')
+def search_for():
+    query = flask.request.args.get('search')
+    result = namedtuple('result', ['val', 'btn'])
+    vals = [("Article1", 60), ("Article2", 50), ("Article 3", 40)]
+    # below is a very simple search algorithm to filter vals based on user input:
+    html = flask.render_template('results.html', results=[result(a, b) for a, b in vals if query.lower() in a.lower()])
+    return flask.jsonify({'results': html})
 
 @app.route("/egg/")
 @app.route("/easter/")
@@ -147,131 +286,8 @@ def lost(path):
 
 
 ################################################################################
-# Requests
-################################################################################
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        if current_user.is_authenticated:
-            return redirect(url_for("lost"))
-        return redirect(url_for("welcome"))
-    login = request.form["uname"]
-    password = request.form["psw"]
-    if not db.check_password(password, login):
-        # TODO add message that login was unsuccesful & keep username
-        return redirect(url_for("welcome"))
-
-    user = User.query.filter_by(Login=login).first()
-    login_user(user)
-    return redirect(url_for("home"))
-
-
-@app.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for("welcome"))
-
-
-'''
-@app.route("/profile/<name>/settings")
-@app.route("/profiles/<name>/settings")
-@app.route("/user/<name>/settings")
-@app.route("/users/<name>/settings")
-@login_required
-def profile_settings(name):
-    if current_user.username != name:
-        return render_template("tresspassing_page.html")
-    return name + " settings page."
-    # TODO return settings
-
-
-@app.route("/group/<name>/settings")
-@app.route("/groups/<name>/settings")
-@login_required
-def group_settings(name):
-    if current_user.id != group.owner:
-        return render_template("tresspassing_page.html")
-    return name + " settings page."
-    # TODO return settings
-
-@app.route("/group/<name>/notifications")
-@app.route("/groups/<name>/notifications")
-@login_required
-def group_settings(name):
-    if current_user.id != group.owner:
-        return render_template("tresspassing_page.html")
-    return name + " settings page."
-    # TODO return settingsň
-
-@app.route("/group/<name>/<thread>")
-@app.route("/groups/<name>/<thread>")
-@login_required
-def see_posts(name):
-    if current_user.id != group.owner:
-        return render_template("tresspassing_page.html")
-    return name + " settings page."
-    # TODO return settings
-
-
-@app.route("/group/<name>/<thread>")
-@app.route("/groups/<name>/<thread>")
-@login_required
-def increment(name):
-    if current_user.id != group.owner:
-        return render_template("tresspassing_page.html")
-    return name + " settings page."
-    # TODO return settings
-
-@app.route("/group/<name>/<thread>")
-@app.route("/groups/<name>/<thread>")
-@login_required
-def decrement(name):
-    if current_user.id != group.owner:
-        return render_template("tresspassing_page.html")
-    return name + " settings page."
-    # TODO return settings
-
-@app.route("/group/<name>/<thread>")
-@app.route("/groups/<name>/<thread>")
-@login_required
-def send_message(name):
-    if current_user.id != group.owner:
-        return render_template("tresspassing_page.html")
-    return name + " settings page."
-    # TODO return settings
-
-@app.route("/group/<name>/<thread>")
-@app.route("/groups/<name>/<thread>")
-@login_required
-def create_group(name):
-    if current_user.id != group.owner:
-        return render_template("tresspassing_page.html")
-    return name + " settings page."
-    # TODO return settings
-
-
-@app.route("/group/<name>/<thread>")
-@app.route("/groups/<name>/<thread>")
-@login_required
-def create_thread(name):
-    if current_user.id != group.owner:
-        return render_template("tresspassing_page.html")
-    return name + " settings page."
-    # TODO return settings
-'''
-
-
-################################################################################
 # Supporting functions
 ################################################################################
-
-@app.before_request
-def make_session_permanent():
-    session.permanent = True
-    app.permanent_session_lifetime = timedelta(hours=1)
-    session.modified = True
-
 
 @app.before_request
 def enforce_https():
@@ -281,26 +297,16 @@ def enforce_https():
         return redirect(url, code=code)
 
 
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(hours=1)
+    session.modified = True
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
-# WE don't know if it works
-@app.route('/search', methods=['GET'])
-def search():
-    return flask.render_template('search.html')
-
-
-# WE don't know if it works
-@app.route('/search_result')
-def search_for():
-    query = flask.request.args.get('search')
-    result = namedtuple('result', ['val', 'btn'])
-    vals = [("Article1", 60), ("Article2", 50), ("Article 3", 40)]
-    # below is a very simple search algorithm to filter vals based on user input:
-    html = flask.render_template('results.html', results=[result(a, b) for a, b in vals if query.lower() in a.lower()])
-    return flask.jsonify({'results': html})
 
 
 if __name__ == "__main__":
