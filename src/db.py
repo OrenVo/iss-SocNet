7f0ccbdddb998245ff2ec4da02d1832806eda25b
Vojtěch Ulej <xulejv00@stud.fit.vutbr.cz>
2020-11-30 21:18:04 +0100

    thread_page some changes

--- src/db.py
+++ src/db.py
@@ -202,7 +202,7 @@ class DB:
         for applicant in applicants:
             user = self.db.session.query(User).filter_by(ID=applicant.User).first()
             if user:
-                users.append(user)
+                users.append((user, ))
             else:
                 eprint(f'[Error] Database inconsistency error. User in Applications table with id: {applicant.User} doesn\'t exist.')
         return users
