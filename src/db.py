5238d3d297aea0f22fa00ef7e35bac6adf00dffb
Vojtěch Ulej <xulejv00@stud.fit.vutbr.cz>
2020-12-01 17:38:37 +0100

    Revert "search case insensitive"
    This reverts commit 0df24978

--- src/db.py
+++ src/db.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 from src.error import eprint
-from sqlalchemy import func
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from configparser import ConfigParser
@@ -152,7 +151,7 @@ class DB:
             group = self.db.session.query(Group).filter_by(ID=mem.Group).first()
             if group:
                 members.append(group)
-
+            ... # get groups
 
         gowner = list()
         gmoderator = list()
@@ -173,8 +172,8 @@ class DB:
             'users': list(),
             'groups': list()
         }
-        users = self.db.session.query(User).filter(func.lower(User.Login).contains(search_word.lower(), autoescape=True)).all()
-        groups = self.db.session.query(Group).filter(func.lower(Group.Name).contains(search_word.lower(), autoescape=True)).all()
+        users = self.db.session.query(User).filter(User.Login.contains(search_word)).all()
+        groups = self.db.session.query(Group).filter(Group.Name.contains(search_word)).all()
         if users:
             for user in users:
                 path = f'/user_picture/{user.ID}/' if user.Image else '/static/pictures/defaults/default_profile_picture.png'
